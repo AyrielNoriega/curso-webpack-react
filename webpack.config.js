6fbb658498f94614383cfde815ae36c8fcac84e6
AyrielNoriega <anoriega2310@gmail.com>
2022-07-29 00:15:15 -0500

    creando script adicionales en package.json

--- webpack.config.js
+++ webpack.config.js
@@ -12,21 +12,27 @@ module.exports = {
     },
     module: {
         rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {loader: 'html-loader'}
+                ]
             }
-        },
-        {
-            test: /\.html$/,
-            use: [
-                {loader: 'html-loader'}
-            ]
-        }
-    ]
+        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true, 
